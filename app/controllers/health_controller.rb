40ccdee29e391263fc261031d90f4ffcf738856c
brenetic <brendan.butler@digital.justice.gov.uk>
2022-01-27 16:26:25 +0000

    Add readiness probe
    This will check that there is an active connection to Redis before
    allowing any traffic to be routed to a new container

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -2,4 +2,10 @@ class HealthController < ApplicationController
   def show
     render plain: 'healthy'
   end
+
+  def readiness
+    if Adapters::RedisCacheAdapter.connection.ping
+      render plain: 'ready'
+    end
+  end
 end
