502b2c1a4b851530d089b8209cce92b4b7ebb157
Natalie Seeto <njseeto@users.noreply.github.com>
2022-12-21 14:20:18 +0000

    Empty service token cache

--- app/services/support/service_token_authoritative_source.rb
+++
@@ -1,7 +0,0 @@
-class Support::ServiceTokenAuthoritativeSource
-  def self.get_public_key(service_slug:, namespace:)
-    adapter = Adapters::KubectlAdapter.new(service_slug: service_slug,
-                                           namespace: namespace)
-    adapter.get_public_key
-  end
-end
