b58de6a361d63270047555d95f7604764c3f3dc0
Natalie Seeto <njseeto@users.noreply.github.com>
2022-12-21 14:55:02 +0000

    Create the basic python service token cache

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM python:3.9-slim-buster
+FROM python:3.11.1-alpine3.17
+
+RUN apk add build-base bash libcurl sqlite sqlite-dev sqlite-libs tzdata
 
 ADD https://storage.googleapis.com/kubernetes-release/release/v1.18.2/bin/linux/amd64/kubectl /usr/local/bin/kubectl
 
@@ -10,6 +12,9 @@ RUN set -x && \
     # Basic check it works.
     kubectl version --client
 
+RUN addgroup -g 1001 -S appgroup && \
+  adduser -u 1001 -S appuser -G appgroup
+
 WORKDIR /python-docker
 
 COPY requirements.txt requirements.txt
@@ -17,4 +22,11 @@ RUN pip3 install -r requirements.txt
 
 COPY . .
 
-CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
+RUN chown -R 1001:appgroup /python-docker
+
+USER 1001
+
+ENV APP_PORT 3000
+EXPOSE $APP_PORT
+
+CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0", "--port=3000"]
--- app.py
+++ app.py
@@ -1,33 +1,59 @@
-from flask import Flask
+from flask import Flask, jsonify
 from markupsafe import escape
-from flask_redis import FlaskRedis
 from dotenv import load_dotenv
+from sentry_sdk import capture_message
+import redis
+import sentry_sdk
 import os
 
 app = Flask(__name__)
-redis_client = FlaskRedis(app)
+REDIS_PROTOCOL = os.getenv("REDIS_PROTOCOL")
+REDIS_AUTH_TOKEN = os.getenv("REDIS_AUTH_TOKEN")
+REDIS_URL = f"{REDIS_PROTOCOL}{os.getenv('REDIS_URL')}"
+
+redis_client = redis.Redis.from_url(url=REDIS_URL, password=REDIS_AUTH_TOKEN)
 load_dotenv()
+
+if os.getenv("SENTRY_DSN") != '':
+    sentry_sdk.init(
+        dsn=os.getenv("SENTRY_DSN")
+    )
+
+
+@app.route("/health")
+def health():
+    return 'healthy'
+
+
+@app.route("/readiness")
+def ready():
+    return 'ready'
 
 
 @app.route("/v3/applications/<service_slug>/namespaces/<namespace>")
 def get_public_key(service_slug, namespace):
     key_name = key(escape(service_slug))
     print('getting key from redis')
-    public_key = redis_client.get(key_name)
 
-    if public_key is None:
-        print('key not found in redis')
-        k8s_public_key = get_k8s_public_key(service_slug, escape(namespace))
-        if k8s_public_key is None:
-            return 'public key does not exist'
-        else:
-            print('found key in k8s')
-            print('saving key to redis')
-            redis_client.set(key_name, k8s_public_key, ex=int(os.getenv('SERVICE_TOKEN_CACHE_TTL')))
-            return k8s_public_key
-    else:
-        print('found key in redis')
-        return public_key
+    try:
+      public_key = redis_client.get(key_name)
+      if public_key is None:
+          print('key not found in redis')
+          k8s_public_key = get_k8s_public_key(service_slug, escape(namespace))
+          if k8s_public_key is None:
+              capture_message(f"{service_slug} not found in k8s")
+              return jsonify({ "token": 'public key does not exist' })
+          else:
+              print('found key in k8s')
+              print('saving key to redis')
+              redis_client.set(key_name, k8s_public_key, ex=int(os.getenv('SERVICE_TOKEN_CACHE_TTL')))
+              return jsonify({ "token": k8s_public_key })
+      else:
+          print('found key in redis')
+          return jsonify({ "token": public_key.decode("utf-8") })
+    except Exception as e:
+      capture_message(str(e))
+      return jsonify({ "token": '' })
 
 
 def key(service_slug):
@@ -36,10 +62,14 @@ def key(service_slug):
 
 def get_k8s_public_key(service_slug, namespace):
     print('getting key from k8s')
-    command = k8s_command(service_slug, namespace)
-    stream = os.popen(command)
-    output = stream.read()
-    return output
+    try:
+      command = k8s_command(service_slug, namespace)
+      stream = os.popen(command)
+      output = stream.read()
+      return output
+    except:
+      capture_message(f"k8s could not get key {service_slug}")
+      return None
 
 
 def k8s_command(service_slug, namespace):
@@ -58,4 +88,4 @@ def k8s_command(service_slug, namespace):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
